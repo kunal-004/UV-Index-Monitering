50068eee39155aec9dba7cf8963cff1a440b92e5
Kunal Pitale <kunal.212034105@vcet.edu.in>
2024-10-01 11:32:55 +0530

    config

--- frontend/src/components/UVDashboard.jsx
+++ frontend/src/components/UVDashboard.jsx
@@ -1,38 +1,38 @@
-import { useEffect, useState } from "react";
-import { Line } from "react-chartjs-2";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { Line } from "react-chartjs-2";
+// import axios from "axios";
 
-const UVDashboard = () => {
-  const [uvData, setUvData] = useState([]);
+// const UVDashboard = () => {
+//   const [uvData, setUvData] = useState([]);
 
-  useEffect(() => {
-    const fetchUVData = async () => {
-      const response = await axios.get("/api/UvData/today");
-      setUvData(response);
-      console.log(response.data);
-    };
+//   useEffect(() => {
+//     const fetchUVData = async () => {
+//       const response = await axios.get("/api/UvData/today");
+//       setUvData(response);
+//       console.log(response.data);
+//     };
 
-    fetchUVData();
-  }, []);
+//     fetchUVData();
+//   }, []);
 
-  const data = {
-    labels: uvData.map((item) => new Date(item.createdAt).toLocaleTimeString()),
-    datasets: [
-      {
-        label: "UV Index",
-        data: uvData.map((item) => item.uvIndex),
-        borderColor: "rgba(75, 192, 192, 1)",
-        borderWidth: 1,
-      },
-    ],
-  };
+//   const data = {
+//     labels: uvData.map((item) => new Date(item.createdAt).toLocaleTimeString()),
+//     datasets: [
+//       {
+//         label: "UV Index",
+//         data: uvData.map((item) => item.uvIndex),
+//         borderColor: "rgba(75, 192, 192, 1)",
+//         borderWidth: 1,
+//       },
+//     ],
+//   };
 
-  return (
-    <div>
-      <h2>UV Index Today</h2>
-      <Line data={data} />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h2>UV Index Today</h2>
+//       <Line data={data} />
+//     </div>
+//   );
+// };
 
-export default UVDashboard;
+// export default UVDashboard;
