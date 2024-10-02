a6d773001d8f13fad0051f1f52ff732e5b4fea0d
Kunal Pitale <kunal.212034105@vcet.edu.in>
2024-10-02 23:21:47 +0530

    vercel config

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,15 @@ app.use(express.json());
 
 app.use("/api", UvRouter);
 
+if ((process.env.NODE_ENV = "production")) {
+  const path = require("path");
+  app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
