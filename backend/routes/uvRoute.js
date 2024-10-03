55dc1b401250b5fbf35059f9af31212d90db405e
Kunal Pitale <kunal.212034105@vcet.edu.in>
2024-10-03 14:32:29 +0530

    added route

--- backend/routes/uvRoute.js
+++ backend/routes/uvRoute.js
@@ -26,4 +26,18 @@ router.get("/UvData/today", async (req, res) => {
   }
 });
 
+router.get("/UvData/latest", async (req, res) => {
+  try {
+    const latestData = await UvData.findOne().sort({ createdAt: -1 });
+    if (latestData) {
+      res.status(200).json(latestData);
+    } else {
+      res.status(404).json({ message: "No UV data found" });
+    }
+  } catch (error) {
+    console.error("Error fetching latest UV data:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
